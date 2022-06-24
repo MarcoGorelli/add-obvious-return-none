2daff82724e7391415503bad10cc6979719c5e01
Marco Gorelli <marcogorelli@protonmail.com>
2022-06-24 13:17:19 +0200

    fixup for abstract methods

--- add_obvious_return_types.py
+++ add_obvious_return_types.py
@@ -5,22 +5,36 @@ import ast
 
 def process_return(node: ast.Return):
     if node is None:
-        return None
-    elif isinstance(node, ast.Constant):
-        return node.value
+        return 'None'
+    elif isinstance(node, ast.Constant) and node.value is None:
+        return 'None'
 
 
 def process_func_def(node: ast.FunctionDef):
+    if node.returns is not None:
+        # already has return type, no need to add one
+        return None
     returns = []
-    walk = ast.walk(node)
-    next(walk)
-    returns.append(process_return(node.returns))
+    walk = list(ast.walk(node))[1:]
+    if len(node.body) == 1 and isinstance(node.body[0], ast.Raise):
+        # might be an abstract method that needs overriding
+        return
+    if (
+            len(node.body) == 2
+            and isinstance(node.body[1], ast.Raise)
+            and isinstance(node.body[0], ast.Expr)
+            and isinstance(node.body[0].value, ast.Constant)
+            and isinstance(node.body[0].value.value, str)
+    ):
+        # might be abstract method with docstring
+        return
+
     for _node in walk:
         if isinstance(_node, ast.FunctionDef):
             return
         if isinstance(_node, ast.Return):
             returns.append(process_return(_node.value))
-    if all(i is None for i in returns):
+    if not returns or all(i == 'None' for i in returns):
         return node
     return None
 
@@ -28,8 +42,8 @@ def rewrite_return_type(src, name, lineno, col_offset):
     lines = src.splitlines(keepends=True)
     before = ''.join(lines[:lineno-1])
     after = ''.join(lines[lineno-1:])
-    pattern = fr'^(def\s+{name}\(.*\)\s*)(:)(.*)'
-    rewrite = re.sub(pattern, '\\1 -> None:\\3', after[col_offset:])
+    pattern = fr'^(def\s+{name}\(.*\)\s*)(:)'
+    rewrite = re.sub(pattern, '\\1 -> None:', after[col_offset:])
     src = before + after[:col_offset] + rewrite
     return src
 
@@ -52,7 +66,7 @@ def rewrite(src):
 
 def main(src, file_):
     ret, src = rewrite(src)
-    if should_rewrite:
+    if ret:
         with open(file_, 'w', encoding='utf-8') as fd:
             fd.write(src)
     return ret
@@ -61,11 +75,11 @@ def main(src, file_):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('files', nargs='*')
-    args = parser.parse_arguments()
+    args = parser.parse_args()
     ret = 0
     for file_ in args.files:
         with open(file_, encoding='utf-8') as fd:
             content = fd.read()
         ret |= main(content, file_)
-    sys.exit(exit)
+    sys.exit(ret)
 
--- tests/main_test.py
+++ tests/main_test.py
@@ -33,3 +33,29 @@ def test_simple():
     ret, result  = rewrite(src)
     assert result == expected
     assert ret
+
+def test_already_has_return():
+    src = (
+        'def foo() -> None:\n'
+        '    a + b\n'
+        '    if True:\n'
+        '        return\n'
+        '    else:\n'
+        '        return None\n'
+    )
+    expected = src
+    ret, result  = rewrite(src)
+    assert result == expected
+    assert not ret
+
+def test_nested_func():
+    src = (
+        'def wrap(func: F) -> F:\n'
+        '    def inner(key: str, *args, **kwds):\n'
+        '        pkey = f"{prefix}.{key}"\n'
+        '        return func(pkey, *args, **kwds)\n'
+    )
+    expected = src
+    ret, result  = rewrite(src)
+    assert result == expected
+    assert not ret
